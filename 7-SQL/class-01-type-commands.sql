42e20dd7ad6417241eb6e693183c0e18226f0920
jeanderson wylis <jeanjeandersonwylisdossantos@gmail.com>
2024-04-12 16:16:52 -0300

    add class 01 commands type

--- 7-SQL/class-01-type-commands.sql
+++ 7-SQL/class-01-type-commands.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS students (
 );        
 
 --DML
-INSERT INTO students (name, CPF, age) VALUES ('Jeanderson Wylis', '148.949.654-88', '18');
+INSERT INTO students (name, CPF, age) VALUES ('TSUKASA FUJII', '148.999.777-90', '12');
+
 
 --DQL
 SELECT * FROM students;
@@ -17,3 +18,30 @@ SELECT name, age FROM students;
 
 select * FROM students
 WHERE id >= 2 LIMIT 3;
+
+SELECT * FROM students
+WHERE age >= 18 and age <= 67;
+
+
+--DML 
+--UPDATE SET WHERE!
+UPDATE students 
+SET age = 36
+WHERE id = 9;
+
+DELETE FROM students
+WHERE id = 10;
+
+
+--TCL
+BEGIN TRANSACTION;
+ROLLBACK;
+COMMIT;
+
+--DDL
+ALTER TABLE students
+ADD COLUMN ANIME TEXT;
+
+UPDATE students 
+SET age = 200
+WHERE id = 12;
