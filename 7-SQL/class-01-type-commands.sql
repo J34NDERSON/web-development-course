cef042a2b6180839d859c0785059d9288d851792
jeanderson wylis <jeanjeandersonwylisdossantos@gmail.com>
2024-04-12 14:53:50 -0300

    add SQL example DQL

--- 7-SQL/class-01-type-commands.sql
+++ 7-SQL/class-01-type-commands.sql
@@ -8,4 +8,12 @@ CREATE TABLE IF NOT EXISTS students (
 );        
 
 --DML
-INSERT INTO students (name, CPF) VALUES ('HASHIRAMA', '148.949.654-16')
+INSERT INTO students (name, CPF, age) VALUES ('Jeanderson Wylis', '148.949.654-88', '18');
+
+--DQL
+SELECT * FROM students;
+
+SELECT name, age FROM students;
+
+select * FROM students
+WHERE id >= 2 LIMIT 3;
