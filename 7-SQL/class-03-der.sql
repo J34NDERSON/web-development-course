2f828fa1565572630bcad1716474bd6ffe45cd32
jeanderson wylis <jeanjeandersonwylisdossantos@gmail.com>
2024-04-24 15:34:43 -0300

    add clients, products, phones and sales data

--- 7-SQL/class-03-der.sql
+++ 7-SQL/class-03-der.sql
@@ -34,3 +34,38 @@ CREATE TABLE IF NOT EXISTS sales (
   FOREIGN KEY (id_client) REFERENCES clients(id) ON DELETE CASCADE
   FOREIGN KEY (id_product) REFERENCES products(id)
 );
+
+INSERT INTO clients (name, cpf) VALUES  ('Mateus', '000.000.000-01'),
+('Pablo', '000.000.000-02'),
+('Ant Carlos', '000.000.000-03');
+
+SELECT *  FROM phones;
+INSERT INTO phones (number, id_client) VALUES 
+('(88) 9900-3555', 1),
+('(88) 9900-3888', 2),
+('(88) 9900-3999', 3);
+
+SELECT * FROM products;
+INSERT INTO products (name, price) VALUES ('PC Gamer', 4000),
+('Gabinete', 350),
+('Monitor', 1000),
+('Cadeira', 1500),
+('Ram 16GB', 200),
+('SSD', 250),
+('RTX', 300),
+('Mouse', 200);
+
+SELECT * FROM sales;
+INSERT INTO sales (id_client, id_product, amount) VALUES
+(1, 2, 3),
+(1, 2, 3),
+(1, 2, 3);
+
+SELECT clients.name, phones.number
+FROM clients INNER JOIN phones
+ON clients.id == phones.id_client;
+
+SELECT clients.name, products.name, sales.amount, products.price, sales.amount * products.price as Total
+FROM clients
+INNER JOIN sales ON sales.id_client == clients.id
+INNER JOIN products on sales.id_product == products.id;
